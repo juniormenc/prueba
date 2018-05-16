0dcba3876f9edd0a14afeac9c6afb41e6e1f0f7d
Junior Mendoza <juniormenc@gmail.com>
2018-05-16 17:15:30 -0500

    make it better

--- src/app/servicios/global/constantes.ts
+++ src/app/servicios/global/constantes.ts
@@ -3,7 +3,7 @@ declare var SERVICIOS;
 let SERVER_PUERTO = (SERVICIOS.PUERTO == '') ? SERVICIOS.PUERTO : ':'+SERVICIOS.PUERTO;
 
 //SERVICIOS.URL="https://healthinn-service.herokuapp.com/api/v1";
-//SERVICIOS.URL="http://localhost:3000";
-SERVICIOS.URL="http://192.168.1.3:3000";
+SERVICIOS.URL="http://localhost:3000";
+//SERVICIOS.URL="http://192.168.1.3:3000";
 
 export const urlServidor = SERVICIOS.URL + SERVER_PUERTO;
