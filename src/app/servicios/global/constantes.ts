3f873671f5460035acaae9d4411e2f3c622294f3
Junior Mendoza <juniormenc@gmail.com>
2018-08-01 23:09:41 -0500

    make it better

--- src/app/servicios/global/constantes.ts
+++ src/app/servicios/global/constantes.ts
@@ -2,7 +2,7 @@ declare var SERVICIOS;
 
 let SERVER_PUERTO = (SERVICIOS.PUERTO == '') ? SERVICIOS.PUERTO : ':'+SERVICIOS.PUERTO;
 
-SERVICIOS.URL="https://h-telemedica-bd.herokuapp.com/";
+SERVICIOS.URL="https://h-telemedica-bd.herokuapp.com";
 //SERVICIOS.URL="http://localhost:3000";
 
 export const urlServidor = SERVICIOS.URL + SERVER_PUERTO;
