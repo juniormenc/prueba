032abc229523ccea8f80fb43533ea2825cb1a009
Junior Mendoza <juniormenc@gmail.com>
2018-09-05 16:19:33 -0500

    make it better

--- src/app/modulos/turno-atencion/editar/turno-atencion-editar.component.ts
+++ src/app/modulos/turno-atencion/editar/turno-atencion-editar.component.ts
@@ -80,7 +80,7 @@ export class TurnoAtencionEditarComponent implements OnInit {
       this.cargarMedicos(this.especialidad);
       this.medico = data.recordSet.element.usuario_id;
       
-      var f_ = this.validar_fecha(data.recordSet.element.fecha_turno);
+      var f_ = data.recordSet.element.fecha_turno;
       this.fecha = f_;
       
       this.total_citas = data.recordSet.element.total_citas;
@@ -110,9 +110,16 @@ export class TurnoAtencionEditarComponent implements OnInit {
     return anio + "-" + mes + "-" + dia;
   }
 
+  validar_fecha_guardar(fecha){
+    var dia = fecha.substr(8, 2);
+    var mes = fecha.substr(5, 2);
+    var anio =  fecha.substr(0, 4);
+    return dia + "-" + mes + "-" + anio;
+  }
+
   modificar(){
     if(this.fecha != null){
-      var f_turno = this.validar_fecha(this.fecha);
+      var f_turno = this.validar_fecha_guardar(this.fecha);
     }
 
     if (this.especialidad == "0" || this.especialidad == null) {
